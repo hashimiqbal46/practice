31e874de90e44e1e9b93528b2c23352259e4f922
Hashim Iqbal <hashim.devsinc@gmail.com>
2019-02-14 17:51:00 +0500

    after updating post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,34 @@
 class PostsController < ApplicationController
+  
+  before_action :find_post, only: [:edit, :show, :update, :destroy]
   def index
+  	@posts = Post.all
+  end
+
+  def new
+  	@post = Post.new
+  end
+
+  def update
+  	if @post.update(post_params)
+  		redirect_to @post
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+  	@post.destroy
+  	redirect_to posts_path
+  end
+
+  private
+
+  def find_post
+  	@post = Post.find(params[:id])
+  end
+
+  def post_params
+  	params.require(:post).permit(:title,:detail)
   end
 end
